458c6d6d2eea6f5d640e82a338a6cc6c3423cd96
Jaime-jpg <jaime.a.p@protonmail.com>
2023-11-27 23:17:35 +0100

    eliminados comentarios innecesarios

--- L06_Extranjería/src/extranjeria.py
+++ L06_Extranjería/src/extranjeria.py
@@ -15,7 +15,6 @@ class RegistroExtranjeria(typing.NamedTuple):
 
 
 def lee_datos_extranjeria(ruta_fichero: str) -> list[RegistroExtranjeria]:
-    # recibe la ruta del fichero CSV, devolviendo una lista de tuplas de tipo RegistroExtranjeria con toda la información contenida en el fichero.
     with open(ruta_fichero, encoding="utf-8") as f:
         archivo = csv.reader(f)
         next(archivo)  # saltamos la cabecera
@@ -26,14 +25,12 @@ def lee_datos_extranjeria(ruta_fichero: str) -> list[RegistroExtranjeria]:
 
 
 def numero_nacionalidades_distintas(registros: list[RegistroExtranjeria]) -> int:
-    # recibe una lista de tuplas de tipo RegistroExtranjeria y devuelve el número de nacionalidades distintas presentes en los registros de la lista recibida como parámetro.
     return len({i.pais for i in registros})
 
 
 def secciones_distritos_con_extranjeros_nacionalidades(
     registros: list[RegistroExtranjeria], paises: set[str]
 ) -> list[tuple[str, str]]:
-    # recibe una lista de tuplas de tipo RegistroExtranjeria y un conjunto de cadenas con nombres de países, y devuelve una lista de tuplas (distrito, seccion) con los distritos y secciones en los que hay extranjeros del conjunto de paises dado como parámetro. La lista de tuplas devuelta estará ordenada por distrito.
     # no hace falta "key=" porque por defecto se selecciona el primer elemento de la tupla
     return sorted(
         {(i.distrito, i.seccion) for i in registros if i.pais.upper() in paises}
@@ -41,7 +38,6 @@ def secciones_distritos_con_extranjeros_nacionalidades(
 
 
 def total_extranjeros_por_pais(registros: list[RegistroExtranjeria]) -> dict[str, int]:
-    # recibe una lista de tuplas de tipo RegistroExtranjeria y devuelve un diccionario de tipo {str:int} en el que las claves son los países y los valores son el número total de extranjeros (tanto hombres como mujeres) de cada país.
     extranjeros_por_pais = typing.DefaultDict(int)
     for i in registros:
         extranjeros_por_pais[i.pais] += i.hombres + i.mujeres
@@ -52,7 +48,6 @@ def total_extranjeros_por_pais(registros: list[RegistroExtranjeria]) -> dict[str
 def top_n_extranjeria(
     registros: list[RegistroExtranjeria], n: int = 3
 ) -> list[tuple[str, int]]:
-    # recibe una lista de tuplas de tipo RegistroExtranjeria y devuelve una lista de tuplas (pais, numero_extranjeros) con los n países de los que hay más población extranjera en los registros pasados como parámetros.
     poblacion_por_pais = typing.DefaultDict(int)
     for i in registros:
         poblacion_por_pais[i.pais] += i.hombres + i.mujeres
@@ -65,7 +60,6 @@ def top_n_extranjeria(
 
 
 def barrio_mas_multicultural(registros: list[RegistroExtranjeria]) -> str:
-    # recibe una lista de tuplas de tipo RegistroExtranjeria y devuelve el nombre del barrio en el que hay un mayor número de países de procedencia distintos.
     paises_por_barrio = typing.DefaultDict(set[str])
 
     for i in registros:
@@ -78,7 +72,6 @@ def barrio_con_mas_extranjeros(
     registros: list[RegistroExtranjeria],
     tipo: typing.Literal["HOMBRES", "MUJERES", None] = None,
 ) -> str:
-    # recibe una lista de tuplas de tipo RegistroExtranjeria y devuelve el nombre del barrio en el que hay un mayor número de extranjeros, bien sea en total (tanto hombres como mujeres) si tipo tiene el valor None, bien sea de hombres si tipo es 'Hombres', o de mujeres si tipo es 'Mujeres'.
     extranjeros_por_barrio = typing.DefaultDict(int)
 
     if tipo is None:
@@ -97,7 +90,6 @@ def barrio_con_mas_extranjeros(
 def pais_mas_representado_por_distrito(
     registros: list[RegistroExtranjeria],
 ) -> dict[str, str]:
-    # recibe una lista de tuplas de tipo RegistroExtranjeria y devuelve un diccionario de tipo {str:str} en el que las claves son los distritos y los valores los países de los que hay más extranjeros residentes en cada distrito.
     distritos = sorted({i.distrito for i in registros})
     pais_por_distrito = {}
 
--- L06_Extranjería/src/extranjeria_test.py
+++ L06_Extranjería/src/extranjeria_test.py
@@ -6,7 +6,6 @@ RUTA_CSV = pathlib.Path(__file__).parents[1] / "data" / "extranjeriaSevilla.csv"
 
 
 def test_lee_datos_extranjeria() -> None:
-    # recibe la ruta del fichero CSV, devolviendo una lista de tuplas de tipo RegistroExtranjeria con toda la información contenida en el fichero.
     datos = extranjeria.lee_datos_extranjeria(str(RUTA_CSV))
     print(f"Leídos {len(datos)} registros")
     print("Tres primeros registros:")
@@ -18,14 +17,12 @@ def test_lee_datos_extranjeria() -> None:
 
 
 def test_numero_nacionalidades_distintas() -> None:
-    # recibe una lista de tuplas de tipo RegistroExtranjeria y devuelve el número de nacionalidades distintas presentes en los registros de la lista recibida como parámetro.
     datos = extranjeria.lee_datos_extranjeria(str(RUTA_CSV))
     resultado = extranjeria.numero_nacionalidades_distintas(datos)
     print(f"Hay {resultado} distintas en los datos.")
 
 
 def test_secciones_distritos_con_extranjeros_nacionalidades() -> None:
-    # recibe una lista de tuplas de tipo RegistroExtranjeria y un conjunto de cadenas con nombres de países, y devuelve una lista de tuplas (distrito, seccion) con los distritos y secciones en los que hay extranjeros del conjunto de paises dado como parámetro. La lista de tuplas devuelta estará ordenada por distrito.
     datos = extranjeria.lee_datos_extranjeria(str(RUTA_CSV))
     resultado = extranjeria.secciones_distritos_con_extranjeros_nacionalidades(
         datos, {"ALEMANIA", "ITALIA"}
@@ -38,7 +35,6 @@ def test_secciones_distritos_con_extranjeros_nacionalidades() -> None:
 
 
 def test_total_extranjeros_por_pais() -> None:
-    # recibe una lista de tuplas de tipo RegistroExtranjeria y devuelve un diccionario de tipo {str:int} en el que las claves son los países y los valores son el número total de extranjeros (tanto hombres como mujeres) de cada país.
     datos = extranjeria.lee_datos_extranjeria(str(RUTA_CSV))
     resultado = extranjeria.total_extranjeros_por_pais(datos)
     print("Mostrando el número de residentes para algunos países de procedencia:")
@@ -47,7 +43,6 @@ def test_total_extranjeros_por_pais() -> None:
 
 
 def test_top_n_extranjeria() -> None:
-    # recibe una lista de tuplas de tipo RegistroExtranjeria y devuelve una lista de tuplas (pais, numero_extranjeros) con los n países de los que hay más población extranjera en los registros pasados como parámetros.
     datos = extranjeria.lee_datos_extranjeria(str(RUTA_CSV))
     resultado = extranjeria.top_n_extranjeria(datos, 5)
     print("Mostrando los 5 países de los que proceden más residentes:")
@@ -55,14 +50,12 @@ def test_top_n_extranjeria() -> None:
 
 
 def test_barrio_mas_multicultural() -> None:
-    # recibe una lista de tuplas de tipo RegistroExtranjeria y devuelve el nombre del barrio en el que hay un mayor número de países de procedencia distintos.
     datos = extranjeria.lee_datos_extranjeria(str(RUTA_CSV))
     resultado = extranjeria.barrio_mas_multicultural(datos)
     print(f"El barrio más multicultural de Sevilla es {resultado}")
 
 
 def test_barrio_con_mas_extranjeros() -> None:
-    # recibe una lista de tuplas de tipo RegistroExtranjeria y devuelve el nombre del barrio en el que hay un mayor número de extranjeros, bien sea en total (tanto hombres como mujeres) si tipo tiene el valor None, bien sea de hombres si tipo es 'Hombres', o de mujeres si tipo es 'Mujeres'.
     datos = extranjeria.lee_datos_extranjeria(str(RUTA_CSV))
 
     resultado_1 = extranjeria.barrio_con_mas_extranjeros(datos)
@@ -75,7 +68,6 @@ def test_barrio_con_mas_extranjeros() -> None:
 
 
 def test_pais_mas_representado_por_distrito() -> None:
-    # recibe una lista de tuplas de tipo RegistroExtranjeria y devuelve un diccionario de tipo {str:str} en el que las claves son los distritos y los valores los países de los que hay más extranjeros residentes en cada distrito.
     datos = extranjeria.lee_datos_extranjeria(str(RUTA_CSV))
     resultado = extranjeria.pais_mas_representado_por_distrito(datos)
     for i, j in resultado.items():
