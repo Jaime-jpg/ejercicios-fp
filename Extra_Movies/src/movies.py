afa0515d0f0b2e4c1275a69a057aa9652814be85
Jaime <jaime.a.p@protonmail.com>
2023-11-29 20:14:50 +0100

    terminada tarea extra movies

--- Extra_Movies/src/movies.py
+++ Extra_Movies/src/movies.py
@@ -1,6 +1,8 @@
 import typing
-from datetime import date, datetime
+from datetime import date
 import csv
+import collections
+
 
 # import collections
 #
@@ -34,25 +36,36 @@ Pelicula = typing.NamedTuple(
     ],
 )
 
+
 # opinión: usar sets para los géneros me parece una mala idea pues ocupan
 # bastante espacio en la memoria y es lento iterar sobre ellos.
 # Una mejor solución creo que sería usar tuplas, pues solucionan estos dos
 # problemas de los sets y los géneros no son algo que se vaya a modificar
 # Adicionalmente, los géneros se deberían representar con un Enum y no como
 # cadenas
+def leer_diccionario_generos(nombre_archivo_generos: str) -> dict[str, set[str]]:
+    generos_por_pelicula = typing.DefaultDict(set[str])
+
+    with open(nombre_archivo_generos, encoding="utf-8") as f:
+        archivo = f.readlines()[1:]  # omitimos la cabecera con [1:]
+
+        for linea in archivo:
+            # [:-1] elimina el caracter '\n' del final de la linea
+            id_, generos = linea[:-1].split(": ")
+
+            # También se prodría hacer con una asignación, es decir,
+            # generos_por_pelicula[id_] = set(generos.split(", "))
+            # Si no hubiera ids repetidas en el csv.
+            # En ese caso, no haría falta DefaultDict
+            generos_por_pelicula[id_].update(generos.split(", "))
+
+    return generos_por_pelicula
 
 
 def leer_peliculas(
     nombre_archivo_peliculas: str, nombre_archivo_generos: str
 ) -> list[Pelicula]:
-    generos_por_pelicula = {}
-
-    with open(nombre_archivo_generos, encoding="utf-8") as f:
-        archivo = f.readlines()[1:]  # omitimos la cabecera con [1:]
-
-        for i in archivo:
-            id_, generos = i.split(": ")
-            generos_por_pelicula[id_] = set(generos.split(", "))
+    generos_por_pelicula = leer_diccionario_generos(nombre_archivo_generos)
 
     peliculas = []
 
@@ -60,7 +73,7 @@ def leer_peliculas(
         archivo = csv.reader(f)
         next(archivo)  # saltamos la cabecera
 
-        for i in archivo:
+        for linea in archivo:
             (
                 id_,
                 title,
@@ -69,13 +82,14 @@ def leer_peliculas(
                 vote_average,
                 popularity,
                 adult,
-            ) = i
+            ) = linea
             peliculas.append(
                 Pelicula(
                     id_,
                     title,
                     original_language,
-                    # equivalente a: datetime.strptime(release_date, "%Y-%m-%d").date()
+                    # equivalente a:
+                    # datetime.strptime(release_date, "%Y-%m-%d").date()
                     date.fromisoformat(release_date),
                     float(vote_average),
                     int(popularity),
@@ -87,5 +101,44 @@ def leer_peliculas(
     return peliculas
 
 
-def genero_mas_frecuente():
-    pass
+def genero_mas_frecuente(movies: list[Pelicula]) -> tuple[str, int]:
+    # otra forma de generar frecuencia_generos:
+    #
+    # frecuencia_generos = typing.DefaultDict(int)
+    # for pelicula in movies:
+    #     for genero in pelicula.genres:
+    #         frecuencia_generos[genero] += 1
+
+    frecuencia_generos = collections.Counter(
+        (genero for pelicula in movies for genero in pelicula.genres)
+    )
+
+    return max(frecuencia_generos.items(), key=lambda x: x[1])
+
+
+def valoracion(pelicula: Pelicula) -> tuple[int, float]:
+    """función auxiliar para ordenar las películas"""
+    return pelicula.popularity, pelicula.vote_average
+
+
+def mejor_valorada_por_idioma(movies: list[Pelicula]) -> dict[str, Pelicula]:
+    movies.sort(key=valoracion)
+    return {pelicula.original_language: pelicula for pelicula in movies}
+
+
+def media_calificaciones(movies: list[Pelicula], generos: set[str]) -> float:
+    notas = [pelicula.vote_average for pelicula in movies if generos <= pelicula.genres]
+    return sum(notas) / len(notas) if len(notas) > 0 else 0.0
+
+
+def top_n_por_genero(movies: list[Pelicula], n: int) -> dict[str, list[Pelicula]]:
+    movies.sort(key=lambda p: p.vote_average, reverse=True)
+    peliculas_por_generos = typing.DefaultDict(list[Pelicula])
+
+    for pelicula in movies:
+        for genero in pelicula.genres:
+            if len(peliculas_por_generos[genero]) < n:
+                peliculas_por_generos[genero].append(pelicula)
+
+    return peliculas_por_generos
+
--- Extra_Movies/src/movies_test.py
+++ Extra_Movies/src/movies_test.py
@@ -1,13 +1,62 @@
 import pathlib
+import movies
+
 
 RUTA_DATA = pathlib.Path(__file__).parents[1] / "data"
 RUTA_GENEROS = RUTA_DATA / "movies_fp_genres.csv"
 RUTA_PELICULAS = RUTA_DATA / "movies_fp.csv"
 
 
-def main() -> None:
+def test_leer_peliculas() -> None:
+    datos = movies.leer_peliculas(str(RUTA_PELICULAS), str(RUTA_GENEROS))
+    print(len(datos))
+    print(f"Primera: {datos[0]}")
+    print(f"Última: {datos[-1]}")
 
-leer_peliculas( str(RUTA_PELICULAS),str(RUTA_GENEROS))
+
+def test_genero_mas_frecuente() -> None:
+    datos = movies.leer_peliculas(str(RUTA_PELICULAS), str(RUTA_GENEROS))
+    resultado = movies.genero_mas_frecuente(datos)
+    print(f"El género más frecuente es {resultado}")
+
+
+def test_mejor_valorada_por_idioma() -> None:
+    datos = movies.leer_peliculas(str(RUTA_PELICULAS), str(RUTA_GENEROS))
+    resultado = movies.mejor_valorada_por_idioma(datos)
+    print(f"Mejor en español (es): {resultado['es']}")
+
+
+def test_media_calificaciones() -> None:
+    datos = movies.leer_peliculas(str(RUTA_PELICULAS), str(RUTA_GENEROS))
+
+    resultado_1 = movies.media_calificaciones(datos, {"Action", "Adventure", "Fake"})
+    print("'Action', 'Adventure', 'Fake'", resultado_1, sep=": ")
+
+    resultado_2 = movies.media_calificaciones(datos, {"Action", "Adventure"})
+    print("'Action', 'Adventure'", resultado_2, sep=": ")
+
+
+def test_top_n_por_genero() -> None:
+    datos = movies.leer_peliculas(str(RUTA_PELICULAS), str(RUTA_GENEROS))
+    resultado = movies.top_n_por_genero(datos, 2)
+
+    print("Top 2 peliculas de 'Fake'", resultado["Fake"], sep=": ")
+    print("Top 2 peliculas de 'Action'", resultado["Action"], sep=": ")
+
+
+def main() -> None:
+    TESTS = (
+        test_leer_peliculas,
+        test_genero_mas_frecuente,
+        test_mejor_valorada_por_idioma,
+        test_media_calificaciones,
+        test_top_n_por_genero,
+    )
+
+    for test in TESTS:
+        print("=" * 10, test.__name__, "=" * 10)
+        test()
+        print()
 
 
 if __name__ == "__main__":
